b5545aeef65c06f3af48ffc0d06b659fcef36cef
Marina <anisimova.marina.n@gmail.com>
2021-08-25 20:47:37 +0300

    bugfix: fix jumping volume btn in mozila

--- my-app/src/components/VolumeBlock/VolumeBlock.tsx
+++ my-app/src/components/VolumeBlock/VolumeBlock.tsx
@@ -14,6 +14,7 @@ import { maxVolume } from "../../utils/constants/constants";
 const StyledVolumeBlock = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-between;
   margin-right: 5px;
 `
 
